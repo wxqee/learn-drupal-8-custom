c17132528a4c605fd7e618e24e127821eb30d8c0
Robin Wang <robin.wang@otms.com>
2017-07-25 18:14:04 +0800

    create new block with more information display

--- hello/src/Plugin/Block/HelloBlock.php
+++ hello/src/Plugin/Block/HelloBlock.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\hello\Plugin\Block;
 
+use Drupal\user\Entity\User;
 use Drupal\Core\Block\BlockBase;
 
 /**
@@ -8,15 +9,36 @@ use Drupal\Core\Block\BlockBase;
  *
  * @Block(
  *   id = "hello_block",
-* admin_label = @Translation("Hello!") *)
-*/
+ *   admin_label = @Translation("Hello!")
+ * )
+ */
 class HelloBlock extends BlockBase {
 	/**
 	 * {@inheritdoc}
 	 */
 	public function build() {
 		return array(
-			'#markup' => $this->t("Hello World!"),
+			'#markup' => $this->_populate_markup(),
 		);
 	}
+
+	private function _populate_markup() {
+		$user = User::load(\Drupal::currentUser()->id());
+
+		if ($user->get('uid')->value < 1) {
+			return t('Welcome  Visitor!  The current time is: ' . date('m-d-Y h:i:s', time()));
+		} else {
+			$user_information = 'User Name: ' . $user->getUsername() . "<br/>";
+			$user_information .= 'Language: ' . $user->getPreferredLangcode() . "<br/>"; $user_information .= 'Email: ' . $user->getEmail() . "<br/>";
+			$user_information .= 'Timezone: ' . $user->getTimeZone() . "<br/>";
+			$user_information .= 'Created: ' . date('m-d-Y h:i:s', $user->getCreatedTime()) . "<br/>"; $user_information .= 'Updated: ' . date('m-d-Y h:i:s', $user->getChangedTime()) . "<br/>"; $user_information .= 'Last Login:' . date('m-d-Y h:i:s', $user->getLastLoginTime()) . "<br/>";
+			$roles = NULL;
+			foreach($user->getRoles() as $role) {
+				$roles .= $role . ",";
+			}
+			$roles = 'Roles: ' . rtrim($roles, ',');
+			$user_information .= $roles;
+			return $user_information;
+		} // else
+	} // function
 }
